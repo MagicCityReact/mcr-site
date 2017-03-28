f988bd5cebce16bd9951e95837416e0c664009d4
coosamatt <scxcdr34>
2017-03-28 16:49:34 -0500

    Temp landing page listing social networks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.jpg';
 import './App.css';
 
 class App extends Component {
@@ -10,10 +10,25 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to MagicCityReact.com</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Full site and details coming soon, until then join us here:
+          <p>
+            <a href ="https://facebook.com/MagicCityReact">Facebook</a>
+          </p>
+          <p>
+            <a href ="https://twitter.com/MagicCityReact">Twitter</a>
+          </p>
+          <p>
+            <a href ="https://discord.gg/2ykvzUc">Discord</a>
+          </p>
+          <p>
+            <a href ="https://instagram.com/MagicCityReact">Instagram</a>
+          </p>
+          <p>
+            <a href ="https://Github.com/MagicCityReact">GitHub</a>
+          </p>
         </p>
       </div>
     );
